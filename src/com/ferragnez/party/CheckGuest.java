f316273cbe4732c640489237ec59a7f59a87d139
MhZ9191 <hegic91@gmail.com>
2026-04-25 17:52:47 +0200

    Add for & while

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,36 @@
 package com.ferragnez.party;
+import java.util.Scanner;
 
 public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        String[] guestList={"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        
+        System.out.printf("%s%3s","Come ti chiami?"," : ");
+        String currentGuest= input.nextLine();
+
+        boolean isInvited=false;
+
+        //CICLO FOR
+        // for(int i=0;i<guestList.length;i++){
+        //     if(guestList[i].equalsIgnoreCase(currentGuest.trim())){
+        //         isInvited=true;
+        //         break;
+        //     }
+        // }
+
+        //CICLO WHILE
+        int cont=0;
+        while(!isInvited && cont < guestList.length){
+            if(guestList[cont].equalsIgnoreCase(currentGuest.trim())){
+                isInvited=true;
+            }
+            cont++;
+        }
+
+        String message = isInvited ? "Sei nella lista! Passa pure" : "Mi spiace non sei in lista!";
+        System.out.println(message);
+        input.close();
+    }
 }
